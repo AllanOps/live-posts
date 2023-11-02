262aff0cf95317536c6fbd781f3acf3ea444003d
AllanOps <ombongiallan@gmail.com>
2023-11-02 13:17:53 +0300

    Defined and initialized the whole array of listOfPosts with a constructor for the various elements within.

--- live-posts/src/app/post-list/post-list.component.ts
+++ live-posts/src/app/post-list/post-list.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-
+  listOfPosts: Post[] = [
+    new Post(
+      'Nature',
+      'Nature yenyewe ata wewe unanawuo, vitu vulai',
+      '',
+      'test@TestScheduler.com',
+      new Date()
+    ),
+    new Post(
+      'New York',
+      'Concrete jungle where dreams are made of',
+      '',
+      'test@TestScheduler.com',
+      new Date()
+    ),
+    new Post(
+      'Dubai',
+      'The Rose of the desert',
+      '',
+      'test@TestScheduler.com',
+      new Date()
+    ),
+    new Post(
+      'Nairobi',
+      'Green city under the sun',
+      '',
+      'test@TestScheduler.com',
+      new Date()
+    ),
+  ];
+  constructor() {}
 }
